24d7c5a3d8834ce90bb3870dae064d46c129f7d8
Daniel Medina <daniel@medina.com>
2021-06-18 13:16:57 -0500

    :sparkles: Changed navbar, added awards section, whatsapp button, and changed tailwindcss from CDN to npm package

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import "./App.css";
 import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import Section from "./components/Section";
 import Footer from "./components/Footer";
+import Awards from './components/Awards';
+import ContactButton from './components/ContactButton';
 
 function App() {
     return (
         <div className="app">
             <Navigation />
-            <div className="pt-16">
+            <div className="pt-20">
                 <Hero />
             </div>
             <main className="flex justify-center items-center flex-col">
@@ -17,9 +18,10 @@ function App() {
                     img="../images/s.jpg"
                     right
                     color="white"
+                    id="nosotros"
                 >
                     <details className="bg-yellow-100 w-full p-4 mb-12 md:mb-0 mt-4">
-                        <summary>Ver más</summary>
+                        <summary className="cursor-pointer">Ver más</summary>
                         <p>
                             Aprenden desde las bases para dominar la lógica de
                             programación, a través de proyectos de diseño de
@@ -38,6 +40,7 @@ function App() {
                         </p>
                     </details>
                 </Section>
+                <Awards />
                 <Section
                     heading="Nuestra Filosofía"
                     img="../images/ca1.jpg"
@@ -86,6 +89,7 @@ function App() {
                 </Section>
             </main>
             <Footer />
+            <ContactButton />
         </div>
     );
 }
---
+++ src/components/ContactButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactButton = () => {
+    return (
+        <div className="fixed bottom-0 right-0 p-6 shadow-2xl">
+            <div className="icons bg-green-400 flex justify-center items-center rounded-full h-20 w-20">
+                <i class="fab fa-whatsapp text-5xl text-white"></i>
+            </div>
+        </div>
+    )
+}
+
+export default ContactButton
