d37c2f2d8edbd6bffcee66c955946e05d2574aa9
Daniel Medina <daniel@medina.com>
2021-06-25 17:27:56 -0500

    Simplified structure

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Awards from './components/Awards';
 import ContactButton from './components/ContactButton';
 import Benefits from './components/Benefits';
 import Temas from "./components/Temas";
+import Professors from "./components/Professors";
+import Main from './components/Main';
 
 function App() {
     return (
@@ -15,34 +17,9 @@ function App() {
                 <Hero />
             </div>
             <main className="flex justify-center items-center flex-col">
-                <Section
-                    heading="Preparamos a los chicos para tener las herramientas para innovar en tecnología y tener una educación de nivel mundial."
-                    img="../images/s.jpg"
-                    right
-                    color="white"
-                    id="nosotros"
-                >
-                    <details className="bg-yellow-100 w-full p-4 mb-12 md:mb-0 mt-4">
-                        <summary className="cursor-pointer">Ver más</summary>
-                        <p>
-                            Aprenden desde las bases para dominar la lógica de
-                            programación, a través de proyectos de diseño de
-                            videojuegos, apps, 3D, electrónica, etc. obteniendo
-                            un lenguaje universal por medio de una gran variedad
-                            de plataformas.
-                        </p>
-                        <br />
-                        <p>
-                            Debemos moldear a los futuros profesionistas con
-                            pasión en las ciencias de la computación de
-                            innovación y transformación digital. Para que logren
-                            pensar de manera crítica, sean hábiles, con
-                            autoestima, incluyentes, colaboradores, innovadores
-                            y sobre todo expertos en crear y solucionar.
-                        </p>
-                    </details>
-                </Section>
+                <Main />
                 <Awards />
+                <Professors />
                 <Temas />
                 <Benefits />
             </main>
