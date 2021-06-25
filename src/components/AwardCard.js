7fe82edd404b56ae2d42d9b79e7c9a4196a6bbe9
Daniel Medina <daniel@medina.com>
2021-06-25 17:27:30 -0500

    Removed dark background in Awards and made the gap smaller, also added real content and images for each card

--- src/components/AwardCard.js
+++ src/components/AwardCard.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const AwardCard = (props) => {
     return (
-        <div className="h-auto flex flex-col justify-start items-start card p-4 shadow-md hover:shadow-2xl bg-gray-800 rounded-xl transition-all duration-100">
-            <div className="image">
+        <div className="h-auto flex flex-col justify-start items-start card p-4 bg-transparent rounded-xl transition-all duration-100">
+            <div className="image w-full">
                 <img src={props.img} alt="" className="rounded-lg object-cover w-full max-h-48" />
             </div>
-            <div className="text p-4 bg-gray-700 rounded-lg h-full mt-4 w-full">
-                <h1 className="text-3xl font-semibold text-white">
+            <div className="text p-4 bg-gray-600 rounded-lg h-full mt-4 w-full">
+                <h1 className="text-3xl font-semibold text-white py-1">
                     { props.title }
                 </h1>
                 <p className="text-white text-md">
--- src/components/Awards.js
+++ src/components/Awards.js
@@ -6,35 +6,40 @@ const Awards = () => {
         <div id="reconocimientos" className="flex flex-col justify-center items-center border-t-2 border-b-2">
             <div className="awards-section bg-topo">
                 <div className="heading w-full">
-                    <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold text-center py-8 md:py-12 px-4 text-gray-800 tracking-tighter">Competencias nacionales y mundiales</h1>
+                    <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold text-center py-8 md:py-12 px-4 text-gray-800 tracking-tighter">La mejor manera de mprender incluye competencias nacionales y mundiales. Algunos de nuestros logros:</h1>
                 </div>
-                <div className="award-cards grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:px-32 md:px-20 px-8 pb-12">
-                    <AwardCard title="Mejor Mentor" img={`https://source.unsplash.com/random?abstract,texture&id=${Math.random()}`}>
+                <div className="award-cards grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:px-32 md:px-20 px-8 pb-12">
+                    <AwardCard title="Mejor Mentor" img={`/images/mejor-mentor.jpg`}>
                         MakeX Global Championship 2018
+                        <br />
                         Competencia de Automatización de Robots
                         <br />
                         CHINA
                         <details className="pt-2">
                             <summary>Ver más</summary>
                             <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolorem animi blanditiis saepe incidunt recusandae odio repellat obcaecati! Corrupti, ipsum repellat pariatur obcaecati quasi temporibus odit voluptates similique facere? Accusamus.
+                                Este premio es entregado a los profesores que demostraron liderazgo, innovación y superioridad técnica, que llevó al equipo a formar parte de la final en el mundial de robótica en China.
                             </p>
+                            <img src="/images/equipo-2018.jpeg" className="mt-2 rounded-lg" alt="" />
                         </details>
                     </AwardCard>
-                    <AwardCard title="2do Lugar Global (sin Asia)" img={`https://source.unsplash.com/random?abstract,texture&id=${Math.random()}`}>
+                    <AwardCard title="2do Lugar Global (sin Asia)" img={`/images/primaria-2018.jpg`}>
                         MakeX Global Championship 2018
+                        <br />
                         Primaria
+                        <br />
                         Competencia de Automatización de Robots
                         <br />
                         CHINA
                         <details className="pt-2">
                             <summary>Ver más</summary>
                             <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolorem animi blanditiis saepe incidunt recusandae odio repellat obcaecati! Corrupti, ipsum repellat pariatur obcaecati quasi temporibus odit voluptates similique facere? Accusamus.
+                                Este premio es entregado a los competidores cuyo proyecto, desempeño y colaboración en equipo les llevó a completar la mayor cantidad de puntos quedando en el ranking mundial, en 2do lugar, sin cotemplar a la liga China. En una competencia con los mejores países del mundo con más de 2,000 competidores.
                             </p>
+                            <img src="/images/arena-2018.jpeg" className="mt-2 rounded-lg" alt="" />
                         </details>
                     </AwardCard>
-                    <AwardCard title="3er Lugar Mundial" img={`https://source.unsplash.com/random?abstract,texture&id=${Math.random()}`}>
+                    <AwardCard title="3er Lugar Mundial" img={`/images/tercer-lugar.jpg`}>
                         MakeX Global Championship 2019
                         Secundaria
                         <br />
@@ -42,30 +47,42 @@ const Awards = () => {
                         <details className="pt-2">
                             <summary>Ver más</summary>
                             <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolorem animi blanditiis saepe incidunt recusandae odio repellat obcaecati! Corrupti, ipsum repellat pariatur obcaecati quasi temporibus odit voluptates similique facere? Accusamus.
+                                Este premio es entregado a los competidores cuyo proyecto, desempeño y colaboración en equipo, en conjunto con el equipo de Ukrania, a ganar en una competencia contra los mejores equipos del mundo.
+                                <br />
+                                +  60 países <br />
+                                + 150 ciudades <br />
+                                +  10,000 equipos <br />
+                                + 45,000 participantes <br />
                             </p>
+                            <img src="/images/equipo-completo.jpg" className="mt-2 rounded-lg" alt="" />
                         </details>
                     </AwardCard>
-                    <AwardCard title="Primer Lugar México" img={`https://source.unsplash.com/random?abstract,texture&id=${Math.random()}`}>
+                    <AwardCard title="Primer Lugar México" img={`/images/primer-lugar-2019.jpg`}>
                         MakeX Nacional 2018 y 2019
                         Primaria y Secundaria
                         <br />
                         CDMX
                         <details className="pt-2">
-                            <summary>Ver más</summary>
+                            <summary className="cursor-pointer">Ver más</summary>
                             <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolorem animi blanditiis saepe incidunt recusandae odio repellat obcaecati! Corrupti, ipsum repellat pariatur obcaecati quasi temporibus odit voluptates similique facere? Accusamus.
+                                <img src="/images/primer-lugar-2018.jpg" className="my-2 rounded-xl" alt="" />
+                                2018
+                                Los alumnos de Coding Academy participaron en la competencia automatización de robots nacional MakeX Robotics Competition, en la categoría de primaria y secundaria donde se ganó el primer lugar respectivamente. Tomando el pase a representar e México en la competencia global en China en 2018.
+                                <br /><br />
+                                2019
+                                Se vuelve a participar con más de 300 competidores de toda la República, pasando todos los equipos  a la final en su categría de Primaria y Secundaria, respectivamente.
+                                Tomando el pase a representar e México en la competencia global en China en 2019.
                             </p>
                         </details>
                     </AwardCard>
-                    <AwardCard title="Primer Lugar Regional" img={`https://source.unsplash.com/random?abstract,texture&id=${Math.random()}`}>
+                    <AwardCard title="Primer Lugar Regional" img={`/images/tmr.jpeg`}>
                         TMR Regional Kids Challenge 2018 Primaria
                         <br />
                         Monterrey, N.L., México
                         <details className="pt-2">
                             <summary>Ver más</summary>
                             <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolorem animi blanditiis saepe incidunt recusandae odio repellat obcaecati! Corrupti, ipsum repellat pariatur obcaecati quasi temporibus odit voluptates similique facere? Accusamus.
+                                Competencia con chicos de entre 8 y 13 años, dónde se debía recoger piezas con temporizador, programando un control remoto para operar con un brazo mecánico en el menor tiempo posible.
                             </p>
                         </details>
                     </AwardCard>
@@ -77,7 +94,7 @@ const Awards = () => {
                         <details className="pt-2">
                             <summary>Ver más</summary>
                             <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolorem animi blanditiis saepe incidunt recusandae odio repellat obcaecati! Corrupti, ipsum repellat pariatur obcaecati quasi temporibus odit voluptates similique facere? Accusamus.
+                                Competencia Virtual organizada entre alumnos de la Academia, con más de 60 participantes, donde se entregaron premios a los primeros 3 lugares y reconocimientos especiales a los proyectos más prometedores. Inspirando a los chicos a desarrollar material propio en su tiempo libre.
                             </p>
                         </details>
                     </AwardCard>
